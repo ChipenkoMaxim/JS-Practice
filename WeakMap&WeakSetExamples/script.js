10a503e1577fe392a786c75ab3b926b2e9cea011
ChipenkoMaxim <chipenkomaxim1@gmail.com>
2021-07-05 15:23:13 +0700

    Add WeakMap and WeakSet Examples

---
+++ WeakMap&WeakSetExamples/script.js
@@ -0,0 +1,27 @@
+const messages = [
+  { text: 'Hello', from: 'John' },
+  { text: 'How goes?', from: 'John' },
+  { text: 'See you soon', from: 'Alice' },
+];
+
+const readMessages = new WeakSet();
+
+// Два сообщения были прочитаны
+readMessages.add(messages[0]);
+readMessages.add(messages[1]);
+// readMessages содержит 2 элемента
+
+// ...давайте снова прочитаем первое сообщение!
+readMessages.add(messages[0]);
+// readMessages до сих пор содержит 2 элемента
+
+// Вопрос: было ли сообщение message[0] прочитано?
+console.log(`Read message 0: ${readMessages.has(messages[0])}`); // true
+
+messages.shift();
+// теперь readMessages содержит 1 элемент (хотя технически память может быть очищена позже)
+
+const readMap = new WeakMap();
+
+readMap.set(messages[0], new Date());
+console.log(readMap.get(messages[0]));
